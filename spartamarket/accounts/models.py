858ed722c1b39e5a0c3d0c7c28cb81237261853e
creative.darkstar <creative.darkstar@gmail.com>
2024-04-18 22:05:20 +0900

    Add: added accounts app's func
    accounts app's func: log-in, log-out, sign-up

--- spartamarket/accounts/models.py
+++ spartamarket/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# class UserInfo(models.Model):
+#     name = models.CharField(max_length=50)
+#     nickname = models.CharField(max_length=50)
+#     userid = models.CharField(max_length=50)
+#     password = models.CharField()
+#     email = models.CharField(max_length=50)
+#     src = models.CharField()
+#     create_dt = models.DateTimeField(auto_now_add=True)
+#     level = models.IntegerField(max_length=10)
+
+
+# class CartInfo(models.Model):
+#     user_idx = models.ForeignKey()
+#     content_idx = models.ForeignKey()
+#     create_dt = models.DateTimeField(auto_now_add=True)
+#
+#
+# class FollowInfo(models.Model):
+#     user_idx = models.ForeignKey()
+#     f_user_idx = models.ForeignKey()
+#     create_dt = models.DateTimeField(auto_now_add=True)
--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    get_list_or_404,
+    get_object_or_404,
+    render,
+    redirect,
+)
+from django.contrib.auth import (
+    login as auth_login,
+    logout as auth_logout,
+)
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    PasswordChangeForm,
+)
+from django.contrib.auth.models import User
+from django.views.decorators.http import (
+    require_http_methods,
+    require_POST,
+)
+# from .forms import CustomUserChangeForm
 
-# Create your views here.
+
+def index(request):
+    return render(request, "accounts/index.html")
+
+
+@require_http_methods(["GET", "POST"])
+def log_in(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url = request.GET.get("next") or "index"
+            return redirect(next_url)
+        else:
+            return redirect("accounts:login")
+    else:
+        return render(request, "accounts/log-in.html", context={"form": AuthenticationForm()})
+
+
+@require_POST
+def log_out(request):
+    auth_logout(request)
+    return redirect("index")
+
+
+@require_http_methods(["GET", "POST"])
+def sign_up(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+        else:
+            return redirect("accounts:signup")
+    else:
+        return render(request, "accounts/sign-up.html", context={"form": UserCreationForm()})
+
+
+def user_profile(request, username):
+    return render(request, "accounts/user-profile.html")
+
+
+def user_profile_edit(request):
+    return render(request, "accounts/user-profile-edit.html")
+
+
+@require_POST
+def user_profile_delete(request):
+    if request.user.is_authenticated:
+        user_row = get_object_or_404(User, id=request.user.id)
+        user_row.is_active = 0
+        user_row.save()
+        auth_logout(request)
+        return redirect("index")
