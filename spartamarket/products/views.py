56c4c4b8aa81ac799364dc42608a4e6e8bfbf3c7
creative.darkstar <creative.darkstar@gmail.com>
2024-04-18 23:58:54 +0900

    add: added products app some settings

--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    get_list_or_404,
+    get_object_or_404,
+    render,
+    redirect,
+)
+from django.contrib.auth.models import User
+from django.views.decorators.http import (
+    require_http_methods,
+    require_POST,
+)
 
+
+def products(request):
+    return render(request, "products/products.html")
 # Create your views here.
